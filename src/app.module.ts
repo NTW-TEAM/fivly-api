669d8773faa6c245e03b890d8cb97dfdd70c112d
Noam - Inclinus <inclinus.stack@gmail.com>
2024-03-25 12:37:42 +0100

    feat(api): Add typeorm config to api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './user/user.entity';
+import { UserService } from "./user/user.service";
+import { DataSource } from "typeorm";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Rend le module disponible globalement
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('MYSQL_HOST'),
+        port: +config.get('MYSQL_PORT'), // Le "+" convertit le port en nombre
+        username: config.get('MYSQL_USER'),
+        password: config.get('MYSQL_PASSWORD'),
+        database: config.get('MYSQL_DATABASE'),
+        entities: [User],
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
